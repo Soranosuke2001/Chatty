1b37a806c8512a172147e7213830735025f6eeec
sora <sorazora1.4@gmail.com>
2023-05-04 16:09:38 -0700

    signup layout complete

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,12 +1,24 @@
+import { useRef } from "react";
 import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 
 export function Signup() {
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const imageUrlRef = useRef<HTMLInputElement>(null);
   return (
     <>
       <h1 className="text-3xl font-bold mb-8 text-center">Signup</h1>
       <form className="grid grid-cols-[auto,1fr] gap-x-3 gap-y-5 items-center justify-items-end">
-        <label>Username</label>
-        <Input />
+        <label htmlFor="userName">Username</label>
+        <Input id="userName" pattern="\S*" required ref={usernameRef} />
+
+        <label htmlFor="name">Name</label>
+        <Input id="name" pattern="\S*" required ref={nameRef} />
+
+        <label htmlFor="imageUrl">Profile Image URL</label>
+        <Input id="imageUrl" type="url" ref={imageUrlRef} />
+        <Button type='submit' className='col-span-full'>Sign Up</Button>
       </form>
     </>
   );
