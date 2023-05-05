9022704c6a80e48cae68420b38487c1d81cbcb6a
sora <sorazora1.4@gmail.com>
2023-05-04 21:58:14 -0700

    redirects to login after signup

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,16 +1,17 @@
 import { UseMutationResult, useMutation } from "@tanstack/react-query";
 import axios, { AxiosResponse } from "axios";
 import { ReactNode, createContext, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 type AuthContext = {
-  signup: UseMutationResult<AxiosResponse, unknown, User>
+  signup: UseMutationResult<AxiosResponse, unknown, User>;
 };
 
 type User = {
-    id: string
-    name: string
-    image?: string
-}
+  id: string;
+  name: string;
+  image?: string;
+};
 
 const Context = createContext<AuthContext | null>(null);
 
@@ -23,10 +24,15 @@ type AuthProviderProps = {
 };
 
 export function AuthProvider({ children }: AuthProviderProps) {
-    const signup = useMutation({
-        mutationFn: (user: User) => {
-            return axios.post(`${import.meta.env.VITE_SERVER_URL}/signup`, user)
-        }
-    })
+  const navigate = useNavigate();
+
+  const signup = useMutation({
+    mutationFn: (user: User) => {
+      return axios.post(`${import.meta.env.VITE_SERVER_URL}/signup`, user);
+    },
+    onSuccess() {
+      navigate("/login");
+    },
+  });
   return <Context.Provider value={{ signup }}>{children}</Context.Provider>;
 }
